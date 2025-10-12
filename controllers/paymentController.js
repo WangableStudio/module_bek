782a763a5797f6257300e5fbf0bb018e39a4332b
WangableStudio <wangable.frontend44@gmail.com>
2025-10-12 12:39:51 +0500

    title

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -580,7 +580,7 @@ class PaymentController {
         },
       });
 
-      return payment
+      console.log(payment);
 
       if (!payment) {
         throw ApiError.badRequest(`Платеж с ID ${paymentId} не найден`);
