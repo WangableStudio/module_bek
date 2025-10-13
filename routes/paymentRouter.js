96c906c27c763165e5b2b58bfdfd74f604b72811
WangableStudio <wangable.frontend44@gmail.com>
2025-10-13 13:16:53 +0500

    title

--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -7,6 +7,7 @@ router.post('/init', paymentController.init);
 router.post('/notification', paymentController.notification);
 router.post('/confirm', paymentController.confirm);
 router.post('/payout', paymentController.payout);
+router.post('/GetSbpMembers', paymentController.GetSbpMembers);
 router.get('/state/:paymentId', paymentController.getState);
 
 module.exports = router;
