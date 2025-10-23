9a93cf87ab0f83d39ff6584a5cdf6b633824d3d9
WangableStudio <wangable.frontend44@gmail.com>
2025-10-23 14:40:03 +0500

    ssl

--- routes/contractorsRouter.js
+++ routes/contractorsRouter.js
@@ -5,11 +5,11 @@ const authMiddleware = require('../middleware/middleware')
 
 router.post('/create', authMiddleware, contractorsController.create)
 router.get('/', contractorsController.getAll)
+router.get('/GetSbpMembers', contractorsController.getBankName);
 router.get('/:id', contractorsController.getOne)
 router.put('/:id', contractorsController.update)
 router.delete('/:id', contractorsController.delete)
 
 router.post('/register', contractorsController.registerPartner);
-router.get('/GetSbpMembers', contractorsController.getBankName);
 
 module.exports = router
