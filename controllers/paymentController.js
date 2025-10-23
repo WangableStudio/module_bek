34105388b6d87fc8822cc2db5572df5cd5d9ff1b
WangableStudio <wangable.frontend44@gmail.com>
2025-10-23 19:28:37 +0500

    ssl

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -97,7 +97,6 @@ class PaymentController {
 
       const response = await axios.post(`${TINKOFF_API_URL}/v2/Init`, payload, {
         headers: { "Content-Type": "application/json" },
-        timeout: 30000,
       });
 
       const data = response.data;
